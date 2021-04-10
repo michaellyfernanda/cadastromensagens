60cd84b18e94f1b86373af8d02f5ecef096d9f2f
michaellyfernanda <michaellyfernanda@outlook.com>
2021-04-10 18:36:05 -0300

    Já testado

--- index.js
+++ index.js
@@ -1,91 +1,83 @@
-const express = require ('express')
-const handlebars =  require ('express-handlebars')
-const bodyparser = require ('body-parser')
+const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+const pessoas = [
+                    {
+                        
+                        "id":"101",
+                        "nome" : "Mika MIh",
+                        "peso":"78",
+                        "altura":"1.8"
+                    }, 
+                    {
+                        "id":"102",
+                        "nome":"Mika Segundo",
+                        "peso":"69",
+                        "altura":"1.6"
+                    }
+                  ]
+app.use(bodyParser.json())
 
-// a aplicação sabe que tem que entrar no layout e pegar o main 
-app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-app.set('view engine', 'handlebars')
-
-//informa onde estão as imagens
-app.use(express.static('/public/img'))
-app.use(bodyparser.urlencoded({extended: false}))
-app.use(bodyparser.json())
-
-const mensagens = []
 
 //Read
-//requisição na raiz p consultar o array, http://localhost:3000/
-app.get('/',(req, res) =>{
-    res.send(mensagens)
+app.get('/',(req,res)=>{
+    res.send(pessoas)
 })
 
-//Create
-//abre o handlebar cadmensagem para o cadastro da mensagem
-app.get('/cadmensagem',(req, res) =>{
-    res.render('cadmensagem')
+app.get('/mensagem/:id',(req,res)=>{
+    const id = req.params.id
+    const pessoa = pessoas.filter(function(item){
+        return item.id == id
+    })
+    if (pessoa.length == 0)
+        res.send("Id Inválido")
+    else
+        res.send(pessoa)
 })
-//retorno de sucesso
-app.post('/mensagemcadastrada',(req, res) => {
-   //pega o conteudo do input
-    const mensagem = req.body.texto
-    //valida se o texto é vazio, se for ele não inclue
-    const texto = mensagem.toString()
-    if (texto.length == 0)
-        res.render('mensagem', {msg: 'Mensagem Inválida'})
-    else{
-        mensagens.push(texto)
-        res.render('mensagem',{msg: 'Inclusão Concluída com sucesso!'})
+
+
+//Create com filtro de id ( repetido )
+app.post('/',(req,res)=>{
+    const pessoa = req.body
+    const indice = pessoas.findIndex(function(item){
+        return item.id == pessoa.id
+    })
+    if (indice == -1){
+        res.send("Inclusão realizada com sucesso")
+        } else {
+            res.send("Id já existe")
+        }
+})
+
+
+//Update
+app.put('/:id', (req, res) => {
+    const id = req.params.id
+    const pessoa = pessoas.findIndex(function(item){
+        return item.id == id
+    })
+    if (pessoa > -1){
+        pessoas[pessoa] = req.body
+        res.send('Alterar Realizada com Sucesso')
+    } else {
+        res.send('Id Inválido')
     }
+});
 
+
+//Delete
+app.delete('/:id',(req,res)=>{
+    const id = req.params.id
+    const indice = pessoas.findIndex(function(item){
+        return item.id == id
+    })    
+    if (indice > -1){
+       pessoas.splice(indice,1)
+       res.send('Remoção Realizada com Sucesso')
+    }
+    else
+       res.send("Indíce Inválido")
 })
-
-//consulta codigo da mensagem para update
-app.get('/consultacodigo',(req,res) => {
-    //renderiza a lista
-        res.render('alteramsg',{msg: mensagens})
-        })
-
-//update
-app.get('/altera/:id',(req,res) =>{
-    //recebe o id e manda renderizar o formulario ja encaixando o texto antigo para modificação
-        const id = parseInt(req.params.id)
-        const texto = mensagens[id]
-        //debug para o console
-        console.info("Info1 " + texto)
-        res.render('formaltera',{mid:id, msg:texto})
-        })
-//retorno da alteração
-app.post('/mensagemalterada/:id',(req,res) => {
-            const mensagem = req.body.texto
-            const id = parseInt(req.params.id)
-            if (mensagem.length == 0)
-            res.render('mensagem',{msg:'Mensagem Inválida'})
-            else {
-            mensagens[id]=mensagem
-            res.render('mensagem',{msg:'Alteração Concluída com Sucesso'})
-            }
-            })
-
-
-//remove
-//encaminha para a lista de opções para remoção
-app.get('/delmensagem',(req,res) => {
-    res.render('removemsg',{msg: mensagens})
-    })
-
-
-//remove single
-app.get('/remove/:id',(req,res) =>{
-    //chega como string transformo em inteiro
-    const id = parseInt(req.params.id)
-    //remove a partir do índice 1 elemento
-    mensagens.splice(id,1)
-    res.render('mensagem',{msg:"Remoção Realizada com Sucesso"})
-    })
-
-teste 
-
 
 app.listen(port)
